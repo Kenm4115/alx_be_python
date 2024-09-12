7db1f57f1735118cd823fda069905602dd8a6834
Kenm <kenm4115@gmail.com>
2024-09-12 22:09:20 +0300

    Modified rectangle area and hours to seconds files

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,6 @@
 hours = 2
-seconds = 3600
+seconds = 3600 * hours
 
 # 1 hour = 3600 seconds (60 minutes * 60 seconds)
 
-print(hours, "hour(s)", "is", seconds * 2, "seconds.")
+print(hours, "hour(s)", "is", seconds, "seconds.")
