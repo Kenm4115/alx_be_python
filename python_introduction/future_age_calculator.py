21522d6099b349cc0e56da53f4249b6cb9294d23
Kenm <kenm4115@gmail.com>
2024-09-12 21:54:19 +0300

    Added new files

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+age = input ("How old are you? ")
+year = 2023
+
+print("In 2050 you will be", age + 27, "years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours = 2
+seconds = 3600
+
+# 1 hour = 3600 seconds (60 minutes * 60 seconds)
+
+print(hours, "hour(s)", "is", seconds * 2, "seconds.")
