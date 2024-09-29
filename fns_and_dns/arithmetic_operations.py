e4bfd259a99ec3c915ec853d65ca593f520de2bd
Kenm <kenm4115@gmail.com>
2024-09-29 12:32:54 +0300

    Modified fns and dns

--- fns_and_dns/arithmetic_operations.py
+++ fns_and_dns/arithmetic_operations.py
@@ -1,15 +1,17 @@
 
-def perform_operation():
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input(
-        "Enter the operation (add, substract, multiply, divide): ")
-
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
-
-
-perform_operation()
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        return num1 / num2
+    else:
+        return "Error: Invalid operation. Please use 'add', 'subtract', 'multiply', or 'divide'."
 
 
 """
