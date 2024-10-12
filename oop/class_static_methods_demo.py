35425c9d5e7dbe26d2a394a84c2f26202c35c961
Kenm <kenm4115@gmail.com>
2024-10-12 11:45:24 +0300

    Added oop

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,14 @@
+
+class Calculator:
+    def __init__(self):
+        pass
+
+
+@staticmethod
+def add(a, b):
+    return a + b
+
+
+@classmethod
+def multiply(cls, a, b):
+    return a * b
---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,17 @@
+
+class shape:
+    def area(self):
+        return "NotImplementedError"
+
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        area = length * width
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        # area = π * radius²
